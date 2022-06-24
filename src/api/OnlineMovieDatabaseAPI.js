4b2679d4faf836bd244afa56e9d1cb01b53d41cc
David Shin <xdavidshinx@gmail.com>
2022-06-23 20:13:17 -0700

    Add search API

--- src/api/OnlineMovieDatabaseAPI.js
+++ src/api/OnlineMovieDatabaseAPI.js
@@ -6,50 +6,57 @@ export const BACKDROP_URL = `https://image.tmdb.org/t/p/original`;
 //MOVIES
 
 export const fetchMovieByID = (movie_id) => {
-  return fetch(
-    `${BASE_URL}/movie/${movie_id}?api_key=${API_KEY}&language=en-US`
-  ).then((res) => res.json());
+	return fetch(
+		`${BASE_URL}/movie/${movie_id}?api_key=${API_KEY}&language=en-US`
+	).then((res) => res.json());
 };
 
 export const fetchMovieTrailersByID = (movie_id) => {
-  return fetch(
-    `${BASE_URL}/movie/${movie_id}/videos?api_key=${API_KEY}&language=en-US`
-  ).then((res) => res.json());
+	return fetch(
+		`${BASE_URL}/movie/${movie_id}/videos?api_key=${API_KEY}&language=en-US`
+	).then((res) => res.json());
 };
 
 export const fetchMovieCreditsByID = (media_id) => {
-  return fetch(`${BASE_URL}/movie/${media_id}/credits?api_key=${API_KEY}`).then(
-    (res) => res.json()
-  );
+	return fetch(
+		`${BASE_URL}/movie/${media_id}/credits?api_key=${API_KEY}`
+	).then((res) => res.json());
 };
 
 //SHOWS
 
 export const fetchShowByID = (show_id) => {
-  return fetch(
-    `${BASE_URL}/tv/${show_id}?api_key=${API_KEY}&language=en-US`
-  ).then((res) => res.json());
+	return fetch(
+		`${BASE_URL}/tv/${show_id}?api_key=${API_KEY}&language=en-US`
+	).then((res) => res.json());
 };
 
 export const fetchTVCreditsByID = (media_id) => {
-  return fetch(`${BASE_URL}/tv/${media_id}/credits?api_key=${API_KEY}`).then(
-    (res) => res.json()
-  );
+	return fetch(
+		`${BASE_URL}/tv/${media_id}/credits?api_key=${API_KEY}`
+	).then((res) => res.json());
 };
 
 //TRENDING
 
 export const fetchAllTrending = (type = "movie", time = "day") => {
-  console.log('poop',`${BASE_URL}trending/${type}/${time}?api_key=${API_KEY}`)
-  return fetch(`${BASE_URL}/trending/${type}/${time}?api_key=${API_KEY}`).then(
-    (res) => res.json()
-  );
+	return fetch(
+		`${BASE_URL}/trending/${type}/${time}?api_key=${API_KEY}`
+	).then((res) => res.json());
 };
 
 //PERSON
 
 export const fetchPersonByID = (person_id) => {
-  return fetch(`${BASE_URL}/person/${person_id}?api_key=${API_KEY}`).then(
-    (res) => res.json()
-  );
+	return fetch(
+		`${BASE_URL}/person/${person_id}?api_key=${API_KEY}`
+	).then((res) => res.json());
+};
+
+//SEARCH
+
+export const fetchSearchResults = (query) => {
+	return fetch(
+		`https://api.themoviedb.org/3/search/multi?api_key=${BASE_URL}&language=en-US&query=${query}&page=1&include_adult=false`
+	).then((res) => res.json());
 };
