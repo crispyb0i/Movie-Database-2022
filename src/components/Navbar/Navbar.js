1442c6b2b4ce1d02386dbb31c7fb904530def266
David Shin <david.shin@mirpartners.com>
2023-01-08 14:09:14 -0800

    update logo

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,26 @@
-import "./Navbar.css";
-import { Link } from "react-router-dom";
+import "./Navbar.css"
+import { Link } from "react-router-dom"
+import logo from "../../assets/shinflixLogo.png"
 
 const Navbar = () => {
-  return (
-    <ul className="navbar_container">
-      <div className="navbar_items_left">
-        <li className="navbar_item logo">
-          <Link to="/" style={{ color: "red" }}>
-            SHINFLIX
-          </Link>
-        </li>
-      </div>
-      <div className="navbar_items_right">
-        {/* <li className="navbar_item">Login</li> */}
-      </div>
-    </ul>
-  );
-};
+	return (
+		<ul className="navbar_container">
+			<div className="navbar_items_left">
+				<li className="navbar_item logo">
+					<Link to="/" style={{ color: "red" }}>
+						<img
+							src={logo}
+							alt="logo"
+							style={{ width: "100px", paddingTop: "5px" }}
+						/>
+					</Link>
+				</li>
+			</div>
+			<div className="navbar_items_right">
+				{/* <li className="navbar_item">Login</li> */}
+			</div>
+		</ul>
+	)
+}
 
-export default Navbar;
+export default Navbar
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,129 +1,139 @@
-import React, { useEffect, useState } from "react";
-import {
-  fetchAllTrending,
-  multiSearch,
-} from "../../api/OnlineMovieDatabaseAPI";
-import MediaCard from "../../components/MediaCard/MediaCard";
-import { FiSearch } from "react-icons/fi";
-import { Oval } from "react-loading-icons";
-import "./Home.css";
+import React, { useEffect, useState } from "react"
+import { fetchAllTrending, multiSearch } from "../../api/OnlineMovieDatabaseAPI"
+import MediaCard from "../../components/MediaCard/MediaCard"
+import { FiSearch } from "react-icons/fi"
+import { Oval } from "react-loading-icons"
+import logo from "../../assets/shinflixHomeLogo.png"
+import "./Home.css"
 
 const Home = () => {
-  const [trending, setTrending] = useState([]);
-  const [trendingTime, setTrendingTime] = useState("day");
-  const [loading, setLoading] = useState(true);
-  const [inputValue, setInputValue] = useState("");
-  const [searchResults, setSearchResults] = useState(false);
-  const [searchLoading, setSearchLoading] = useState(false);
+	const [trending, setTrending] = useState([])
+	const [trendingTime, setTrendingTime] = useState("day")
+	const [loading, setLoading] = useState(true)
+	const [inputValue, setInputValue] = useState("")
+	const [searchResults, setSearchResults] = useState(false)
+	const [searchLoading, setSearchLoading] = useState(false)
 
-  useEffect(() => {
-    fetchAllTrending("all", trendingTime)
-      .then((data) => {
-        setTrending(data.results);
-      })
-      .then(() => setLoading(false))
-      .catch((err) => console.log(err));
-  }, [trendingTime]);
+	useEffect(() => {
+		fetchAllTrending("all", trendingTime)
+			.then((data) => {
+				setTrending(data.results)
+			})
+			.then(() => setLoading(false))
+			.catch((err) => console.log(err))
+	}, [trendingTime])
 
-  const inputChangeHandler = (e) => {
-    setInputValue(e.target.value.trim());
-  };
+	const inputChangeHandler = (e) => {
+		setInputValue(e.target.value.trim())
+	}
 
-  const searchHandler = () => {
-    setSearchLoading(true);
-    multiSearch(inputValue)
-      .then((data) => {
-        setSearchResults(data.results);
-      })
-      .then(() => setSearchLoading(false));
-  };
+	const searchHandler = () => {
+		setSearchLoading(true)
+		multiSearch(inputValue)
+			.then((data) => {
+				setSearchResults(data.results)
+			})
+			.then(() => setSearchLoading(false))
+	}
 
-  const buttonClickHandler = (e) => {
-    setTrendingTime(e.target.value);
-  };
+	const buttonClickHandler = (e) => {
+		setTrendingTime(e.target.value)
+	}
 
-  const selectedStyle = {
-    backgroundColor: "black",
-    color: "white",
-  };
+	const selectedStyle = {
+		backgroundColor: "black",
+		color: "white",
+	}
 
-  return (
-    <div className="home_container">
-      {loading ? (
-        <div className="loading_icon">
-          <Oval stroke="black" style={{ textAlign: "center" }} />
-        </div>
-      ) : (
-        <>
-          <h1 className="home_title">SHINFLIX</h1>
+	return (
+		<div className="home_container">
+			{loading ? (
+				<div className="loading_icon">
+					<Oval stroke="black" />
+				</div>
+			) : (
+				<>
+					{/* <h1 className="home_title">SHINFLIX</h1> */}
+					<div style={{ width: "100%", display: "flex" }}>
+						<img
+							src={logo}
+							alt="netflix-font"
+							border="0"
+							style={{
+								width: "400px",
+								margin: "auto",
+								justifyContent: "center",
+							}}
+						/>
+					</div>
 
-          <div className="home_search_input_container">
-            <input
-              type="text"
-              className="home_search_input"
-              onChange={inputChangeHandler}
-              placeholder="Search for movie, TV show, or person"
-              onKeyPress={(e) => {
-                if (e.key === "Enter") searchHandler();
-              }}
-            />
-            <button
-              className="home_search_input_button"
-              onClick={searchHandler}
-            >
-              <FiSearch size={40} />
-            </button>
-          </div>
+					<div className="home_search_input_container">
+						<input
+							type="text"
+							className="home_search_input"
+							onChange={inputChangeHandler}
+							placeholder="Search for movie, TV show, or person"
+							onKeyPress={(e) => {
+								if (e.key === "Enter") searchHandler()
+							}}
+						/>
+						<button
+							className="home_search_input_button"
+							onClick={searchHandler}
+						>
+							<FiSearch size={40} />
+						</button>
+					</div>
 
-          {searchLoading ? (
-            <div className="loading_icon_search">
-              <Oval stroke="black" style={{ textAlign: "center" }} />
-            </div>
-          ) : (
-            <>
-              <div className="search_results_container">
-                <div className="search_results">
-                  {searchResults.length === 0 && <h1>No results found</h1>}
-                  {searchResults &&
-                    searchResults.map((result) => (
-                      <MediaCard media={result} key={result.id} />
-                    ))}
-                </div>
-              </div>
+					{searchLoading ? (
+						<div className="loading_icon_search">
+							<Oval stroke="black" style={{ textAlign: "center" }} />
+						</div>
+					) : (
+						<>
+							<div className="search_results_container">
+								<div className="search_results">
+									{searchResults.length === 0 && <h1>No results found</h1>}
+									{searchResults &&
+										searchResults.map((result) => (
+											<MediaCard media={result} key={result.id} />
+										))}
+								</div>
+							</div>
 
-              <div className="media_container">
-                <div className="container_header">
-                  <h1 className="container_title">Trending</h1>
-                  <button
-                    className="trending_time_button"
-                    onClick={buttonClickHandler}
-                    value={"day"}
-                    style={trendingTime === "day" ? selectedStyle : null}
-                  >
-                    DAY
-                  </button>
-                  <button
-                    className="trending_time_button"
-                    onClick={buttonClickHandler}
-                    value={"week"}
-                    style={trendingTime === "week" ? selectedStyle : null}
-                  >
-                    WEEK
-                  </button>
-                </div>
+							<div className="media_container">
+								<div className="container_header">
+									<h1 className="container_title">Trending</h1>
+									<button
+										className="trending_time_button"
+										onClick={buttonClickHandler}
+										value={"day"}
+										style={trendingTime === "day" ? selectedStyle : null}
+									>
+										DAY
+									</button>
+									<button
+										className="trending_time_button"
+										onClick={buttonClickHandler}
+										value={"week"}
+										style={trendingTime === "week" ? selectedStyle : null}
+									>
+										WEEK
+									</button>
+								</div>
 
-                <div className="home_media_container">
-                  {trending.map((media) => (
-                    <MediaCard media={media} key={media.id} />
-                  ))}
-                </div>
-              </div>
-            </>
-          )}
-        </>
-      )}
-    </div>
-  );
-};
+								<div className="home_media_container">
+									{trending.map((media) => (
+										<MediaCard media={media} key={media.id} />
+									))}
+								</div>
+							</div>
+						</>
+					)}
+				</>
+			)}
+		</div>
+	)
+}
 
-export default Home;
+export default Home
